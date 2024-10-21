5777c9e5c5d7a43a1e2d88485ab2acdbb45d75b9
ancient-miaw <rizqieadriananto@gmail.com>
2024-10-22 01:47:54 +0700

    profile

---
+++ jawa.js
@@ -0,0 +1,25 @@
+/*buat tombol di menu header*/
+function toggleMenu() {
+    const menu = document.getElementById('mobile-menu');
+    const bars = document.querySelectorAll('.bar');
+    menu.classList.toggle('hidden');
+
+if (!menu.classList.contains('hidden')) {
+        menu.classList.add('menu-animate');
+    } else {
+        menu.classList.remove('menu-animate');
+    }
+    
+    bars.forEach(bar => {
+        bar.classList.toggle('active');
+    });
+}
+
+/* projek nya*/
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        alert('Project clicked!');
+    });
+});
